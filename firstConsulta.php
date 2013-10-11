85a25c608a1bd99666e3ad2673ebcfae195dce88
yosselyn <yose.naju@hotmail.com>
2013-10-11 09:36:41 -0500

    Update firstConsulta.php

--- firstConsulta.php
+++ firstConsulta.php
@@ -54,7 +54,7 @@ include("seguridad.php");
         <div id="search" class="box">
             <form action="historial.php" method="get">
                 <div class="box">
-                    <div id="search-input"><span class="noscreen">Search:</span><input type="text" size="30" name="ide" placeholder="Buscar: " /></div>
+                    <div id="search-input"><span class="noscreen">Search:</span><input type="text" size="30" name="ide" placeholder="Buscar" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Buscar'"  /></div>
                     <div id="search-submit"><input type="image" src="design/search-submit.gif" value="OK" /></div>
                 </div>
             </form>
