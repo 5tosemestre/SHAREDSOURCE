d2a4f8e4a09c20731e0f2fb6b1fbce6b9dd59d3c
richi-42 <richi-42@hotmail.com>
2013-10-11 15:56:39 -0500

    lógin y de la busqueda rapida
    realice la lógica del login y de la busqueda rapida, implemente el de
    solo introducir letras y numeros

--- login.php
+++ login.php
@@ -1,25 +1,32 @@
 <?php
+
 include("seguridad.php");
 $users=$_REQUEST["user"];
 $pas=$_REQUEST["pass"];
 include("conexion.php");
-include("subir.php");	
-	
+include("subir.php");
+
 //datos para establecer la conexion con la base de mysql.
 $conexion= mysql_connect($host,$user,$pw);
 mysql_select_db($db,$conexion);
 function quitar($mensaje)
 {
-		$nopermitidos = array("'",'\\','<','>',"\"");
-		$mensaje = str_replace($nopermitidos, "", $mensaje);
-		return $mensaje;
+
+	$nopermitidos = array("'",'\\','<','>',"\"");
+	$mensaje = str_replace($nopermitidos, "", $mensaje);
+	return $mensaje;
 }
+
+
 if(trim($users) != "" && trim($pas) != "")
 {
+
 	// Puedes utilizar la funcion para eliminar algun caracter en especifico
 	//$usuario = strtolower(quitar($HTTP_POST_VARS["usuario"]));
 	//$password = $HTTP_POST_VARS["password"];
 	// o puedes convertir los a su entidad HTML aplicable con htmlentities
+
+
 	$usuario = strtolower(htmlentities($users, ENT_QUOTES));
 	$password = $pas;
 	$consulta=sprintf("SELECT id,usuario,contrasenia,roles_id from usuarios WHERE usuario='%s' and contrasenia ='%s'",
@@ -43,17 +50,21 @@ if(trim($users) != "" && trim($pas) != "")
 }else{
 	 
 
+
 	echo 'Debe especificar un usuario y password';
 	header("location:mensajeIndex.php");
 }
 }
+
 			if($_SESSION["k_nam"]==1 || $_SESSION["k_nam"]==2)
+
 			{
 
 				header("location:mainpage.php");
 				echo $_SESSION['k_username'];
 			}
 		
+
 mysql_close();
 
 ?>
